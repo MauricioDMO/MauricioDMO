f4c5bb0c8dc744a1785c88c14e09e2e3b6f397bc
MauricioDMO <mauriciodmoprogrammer@gmail.com>
2024-07-12 23:37:10 -0600

    Update tailwind.config.js with additional spacing and font options

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,20 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			spacing: {
+				"40ch": "40ch",
+				"48ch": "48ch",
+				"64ch": "64ch",
+				"80ch": "80ch",
+				"96ch": "96ch",
+				"128ch": "128ch",
+			},
+			fontFamily: {
+				"mono": ["Roboto Mono", "monospace"],
+				"raleway": ["Raleway", "sans-serif"],
+			},
+		},
 	},
 	plugins: [],
 }
