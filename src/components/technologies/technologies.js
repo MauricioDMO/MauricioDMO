d3ecf2cb0af0e8f54712165efb020922bfce9e6b
MauricioDMO <mauriciodmoprogrammer@gmail.com>
2024-07-13 21:12:33 -0600

    Create object of technologies

---
+++ src/components/technologies/technologies.js
@@ -0,0 +1,51 @@
+export default {
+  html: {
+    name: "HTML",
+    icon: "/icons/html.svg",
+    page: "https://es.wikipedia.org/wiki/HTML5",
+    color: "#f06529"
+  },
+  css: {
+    name: "CSS",
+    icon: "/icons/css.svg",
+    page: "https://es.wikipedia.org/wiki/Hoja_de_estilos_en_cascada",
+    color: "#2965f1"
+  },
+  vanillaJS: {
+    name: "JavaScript",
+    icon: "/icons/javascript.svg",
+    page: "https://es.wikipedia.org/wiki/JavaScript",
+    color: "#f0db4f"
+  },
+  react: {
+    name: "React",
+    icon: "/icons/react.svg",
+    page: "https://react.dev/",
+    color: "#61dafb"
+  },
+  ionic: {
+    name: "Ionic",
+    icon: "/icons/ionic.svg",
+    page: "https://ionicframework.com/",
+    color: "#3880ff"
+  },
+  astro: {
+    name: "Astro",
+    icon: "/icons/astro.svg",
+    page: "https://astro.build/",
+    color: "#000000"
+
+  },
+  tailwind: {
+    name: "Tailwind",
+    icon: "/icons/tailwind.svg",
+    page: "https://tailwindcss.com/",
+    color: "#06b6d4"
+  },
+  vercel: {
+    name: "Vercel",
+    icon: "/icons/vercel.svg",
+    page: "https://vercel.com/",
+    color: "#000000"
+  },
+}
